c0f8703ca899457c2ab34a7ae19d22dfddff260b
eduardochango <eduardo.chango@udla.edu.ec>
2026-05-14 14:56:38 -0500

    first commit

---
+++ ejercicio.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int numeros[7] = {3,6,9,12,15,18,21};
+    
+    for(int i=0;i<7;i++){
+    printf("| %d", numeros[i]);
+    }
+    printf("|");
+    return 0;
+}
