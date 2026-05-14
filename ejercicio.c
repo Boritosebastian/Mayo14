71c2f9469e7048e75a78c9d3c7de27d278ee6cc7
eduardochango <eduardo.chango@udla.edu.ec>
2026-05-14 15:05:46 -0500

    Segunda version del codigo para imprimir un puntero hacia un vector

--- ejercicio.c
+++ ejercicio.c
@@ -2,10 +2,14 @@
 
 int main() {
     int numeros[7] = {3,6,9,12,15,18,21};
+    int *ptr;
+    ptr = &numeros[0];
+    
+    //Segunda version del codigo para imprimir un puntero hacia un vector
     
     for(int i=0;i<7;i++){
-    printf("| %d", numeros[i]);
+        printf("%d ", numeros[i]);
+        printf("%p \n",*ptr);
     }
-    printf("|");
     return 0;
 }
